a8b97325e29bb9bf705b69c10aa16654fbf13121
nico-bertoli <nicobertoli.1999@gmail.com>
2022-10-01 18:34:23 +0200

    basic dash added

--- Assets/Scripts/Player/Pumpkin.cs
+++ Assets/Scripts/Player/Pumpkin.cs
@@ -1,14 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using xPoke.CustomLog;
 
 public class Pumpkin : Player {
 
-    [SerializeField] float dashSpeed;
-    [SerializeField] float dashDuration;
+    [SerializeField] float dashSpeed = 100f;
+    [SerializeField] float dashDuration = 0.5f;
+
+    private bool canUseAblity = true;
+
+    protected override void Update() {
+
+        Debug.Log(InputManager.Instance.IsAbilityPlayer1Pressed);
+
+        
+
+        if ((playerNumber == PlayerNumber.PlayerOne && InputManager.Instance.IsAbilityPlayer1Pressed)
+            ||
+            (playerNumber == PlayerNumber.PlayerTwo && InputManager.Instance.IsAbilityPlayer2Pressed)) {
+            UseAbility();
+        }
+
+        base.Update();
+    }
 
     protected override void UseAbility() {
-        
+        CustomLog.Log(CustomLog.CustomLogType.PLAYER, "Ability used");
+        canUseAblity = false;
         readInput = false;
 
         _rb.velocity = transform.forward * dashSpeed;
@@ -20,5 +39,6 @@ public class Pumpkin : Player {
         yield return new WaitForSeconds(dashDuration);
         //_rb.velocity = Vector3.zero;
         readInput = true;
+        canUseAblity = true;
     }
 }
