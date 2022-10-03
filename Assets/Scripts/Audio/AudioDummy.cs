88ca3190e7f459878aa17d52a592a7476a23311c
Cristian <cri.ghiorzi@gmail.com>
2022-10-03 16:22:51 +0200

    WWise RTPC test

--- Assets/Scripts/Audio/AudioDummy.cs
+++ Assets/Scripts/Audio/AudioDummy.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class AudioDummy : MonoBehaviour
 {
-    [SerializeField]
-    private AkEvent dummyEvent;
+    private float _timer = 0;
 
     private void Start()
     {
+        StartCoroutine(COTimer());
+    }
 
+    private void Update()
+    {
+
+        if (_timer >= 30)
+            _timer = 30;
+    }
+
+    private IEnumerator COTimer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            _timer++;
+            AkSoundEngine.SetRTPCValue("timer", _timer);
+            Debug.Log(_timer);
+        }
+       
     }
 }
