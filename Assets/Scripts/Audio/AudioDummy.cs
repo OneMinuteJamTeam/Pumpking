849f9e7d8e4eced0a1e17f5217cfec9d12696de4
Cristian <cri.ghiorzi@gmail.com>
2022-10-03 14:53:19 +0200

    AudioTry added

---
+++ Assets/Scripts/Audio/AudioDummy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioDummy : MonoBehaviour
+{
+    [SerializeField]
+    private AkEvent dummyEvent;
+
+    private void Start()
+    {
+
+    }
+}
